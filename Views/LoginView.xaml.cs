5737bbd3602721f2019e9f7b6506cc5281ab1e2a
HIvanovH <hristohristov1050@gmail.com>
2022-09-15 13:29:32 +0300

    .

---
+++ Views/LoginView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Zoo.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        //passwordbox binding is not allowed by security reasons so code-behind is used
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e) 
+        { 
+            if (this.DataContext != null) 
+            { 
+                ((dynamic)this.DataContext).Password = ((PasswordBox)sender).Password; 
+            } 
+        }
+    }
+}
